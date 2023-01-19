454d3ad3fbad7abac346850697da21cfddef2eca
Beatrizvn <beatriz-vn@hotmail.com>
2023-01-19 19:37:49 -0300

    76-envio-de-plano-para-banco-de-dados

--- Bot_teste/src/boto_prof.py
+++ Bot_teste/src/boto_prof.py
@@ -18,11 +18,11 @@ def main():
     dp =  Application.builder().token(TOKEN).build()
 
     dp.add_handler(CommandHandler("help", handlers_prof.help_command))
-    dp.add_handler(CommandHandler("plano_de_ensino", handlers_prof.plano_de_ensino))
     dp.add_handler(CommandHandler("cadastrar_conteudo", handlers_prof.cadastrar_conteudo))
 
     dp.add_handler(handlers_prof.entrada_conversation)
     dp.add_handler(handlers_prof.enviar_planilha_conversation)
+    dp.add_handler(handlers_prof.enviar_plano_conversation)
 
     print("Bot started. . .\n")
     dp.run_polling()
