051186f788ae994b870a8fde2534db9c89f59a33
Beatrizvn <beatriz-vn@hotmail.com>
2023-01-18 21:40:47 -0300

    Envio do Conteudo para o Banco de dados #73

--- Bot_teste/src/conexaoDataBase/db/databaseBOTO.py
+++ Bot_teste/src/conexaoDataBase/db/databaseBOTO.py
@@ -4,11 +4,10 @@ from contextlib import contextmanager
 parametros = dict(
     host='localhost',
     port=3306,
-    user='root',
-    password='211041295', ## usar senha do seu DB
-    database='BOTO'
+    user='username',     ## usar nome do seu usuario
+    password='Beavn/02', ## usar senha do seu DB
+    database='BOT'
 )
-
 @contextmanager
 def nova_con():
     con = connect(**parametros)
